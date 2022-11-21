686788e8f1514b8631f855189551879aad5dac8e
Kevin <kevinsantander@tdf.edu.ar>
2022-11-20 23:28:26 -0300

    Variables para recorrer el arbol-dom

--- index.js
+++ index.js
@@ -17,3 +17,27 @@ btn.addEventListener('click' , ()=> {
   console.log();
 
 });
+
+/*
+let count = document.getElementById('div').children.length;
+console.log(count); //element.children, encuentra los elementos hijos
+
+let fahter = document.getElementById('p2').parentElement.id;
+console.log(fahter); //element.parentElement, encuentra el elemento padre.
+
+
+let prevBro = document.getElementById('p2').previousElementSibling.innerText;
+console.log(prevBro); //element.previousElementSibling, encuentra el hermano anterior.
+
+let nextBro = document.getElementById('p2').nextElementSibling.innerText;
+console.log(nextBro); //element.nextElementSibling, encuentra el siguiente hermano.
+
+let centBro = document.getElementById('p1').nextElementSibling.innerText;
+console.log(centBro);
+
+//Retorna un nodo
+let primerParr = document.querySelector(".p1");
+console.log(primerParr);
+
+let segunParr = document.getElementById('p2');
+console.log(segunParr);*/
